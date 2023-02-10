63916812e9ef14acf3752d8d42feb5a9508b3145
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-02-10 16:55:28 -0300

    Restart game created

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameController : MonoBehaviour
@@ -24,5 +25,10 @@ public class GameController : MonoBehaviour
     public void ShowGameOver()
     {
         gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
     }
 }
