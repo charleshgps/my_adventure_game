360389bbe45ff0ac380df8a93b8d612bf68e91b6
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-02-19 23:25:03 -0300

    bugs corrigidos, inimigo criado, fase 4 em desenvolvimento

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -15,9 +15,8 @@ public class Frog : MonoBehaviour
     public Transform leftCol;
     public Transform headPoint;
     private bool colliding;
-    private LayerMask layer;
+    public LayerMask layer;
 
-    private float jumpForce;
 
     public BoxCollider2D boxCollider2D;
     public CircleCollider2D circleCollider2D;
@@ -32,25 +31,24 @@ public class Frog : MonoBehaviour
     void Update()
     {
         rig.velocity = new Vector2(speed, rig.velocity.y);
-
         colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
-
         if (colliding)
         {
             transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
-            speed *= -1f;
+            speed = -speed;
         }
     }
 
+    bool playerDestroyed = false;
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
             float height = col.contacts[0].point.y - headPoint.position.y;
 
-            if (height > 0)
+            if (height > 0 && !playerDestroyed)
             {
-                rig.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
                 speed = 0;
                 anim.SetTrigger("die");
                 boxCollider2D.enabled = false;
@@ -58,6 +56,12 @@ public class Frog : MonoBehaviour
                 rig.bodyType = RigidbodyType2D.Kinematic;
 
                 Destroy(gameObject, 0.33f);
+            }
+            else
+            {
+                playerDestroyed = true;
+                GameController.instance.ShowGameOver();
+                Destroy(gameObject);
             }
         }
     }
